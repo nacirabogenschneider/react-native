1da82df00223befca371283ea49f0927ca6f20f1
Nacira Bogenschneider <49534732+nacirabogenschneider@users.noreply.github.com>
2020-06-02 23:06:00 +0200

    Update dependencies

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,1 +1,17 @@
-    
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Screen from "../components/Screen";
+
+function LoginScreen() {
+  return (
+    <Screen>
+      <View style={styles.container}></View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default LoginScreen;
