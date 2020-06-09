70a005dc6f5445178c8b58416eedaaa406abdb79
Nacira Bogenschneider <49534732+nacirabogenschneider@users.noreply.github.com>
2020-06-09 16:23:43 +0200

    Update Dependencies

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <ListingsScreen />;
 }
